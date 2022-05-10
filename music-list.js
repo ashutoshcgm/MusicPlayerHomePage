48d489c2c4708eb4873ff700a3aff3e90151931f
ashutoshcgm <ashutoshexlcgm@gmail.com>
2022-05-10 20:17:15 +0530

    first commit

---
+++ music-list.js
@@ -0,0 +1,194 @@
+let allMusic = [
+    {
+        name: "Bhul-Bhulaiya Title Track",
+        artist: "music-1",
+        img: "Music-1",
+        src: "Music-1"
+    },
+    {
+        name: "Dua Karo",
+        artist: "music-2",
+        img: "Music-2",
+        src: "Music-2"
+    },
+    {
+        name: "Jabra Fan",
+        artist: "music-3",
+        img: "Music-3",
+        src: "Music-3"
+    },
+    {
+        name: "Jee Karda",
+        artist: "music-4",
+        img: "Music-4",
+        src: "Music-4"
+    },
+    {
+        name: "Johnny Johnny - Entertainment",
+        artist: "music-5",
+        img: "Music-5",
+        src: "Music-5"
+    },
+    {
+        name: "Jugnu",
+        artist: "music-6",
+        img: "Music-6",
+        src: "Music-6"
+    }
+    ,
+    {
+        name: "Kabhi Tumhe  ",
+        artist: "music-7",
+        img: "Music-7",
+        src: "Music-7"
+    }
+    ,
+    {
+        name: "Mere Yaraa",
+        artist: "music-8",
+        img: "Music-8",
+        src: "Music-8"
+    }
+    ,
+    {
+        name: "Nainowale Ne",
+        artist: "music-9",
+        img: "Music-9",
+        src: "Music-9"
+    }
+    ,
+    {
+        name: "Najaa",
+        artist: "music-10",
+        img: "Music-10",
+        src: "Music-10"
+    },
+    {
+        name: "Baby Justin Bieber",
+        artist: "music-11",
+        img: "Music-11",
+        src: "Music-11"
+    },
+    {
+        name: "Bad-habits : Ed Sheeran",
+        artist: "music-12",
+        img: "Music-12",
+        src: "Music-12"
+    },
+    {
+        name: "Love Yourself",
+        artist: "music-13",
+        img: "Music-13",
+        src: "Music-13"
+    },
+    {
+        name: "Let me love You",
+        artist: "music-14",
+        img: "Music-14",
+        src: "Music-14"
+    },
+    {
+        name: "Industry Baby",
+        artist: "music-15",
+        img: "Music-15",
+        src: "Music-15"
+    },
+    {
+        name: "Perfect-Ed Sheeran",
+        artist: "music-16",
+        img: "Music-16",
+        src: "Music-16"
+    }
+    ,
+    {
+        name: "Shape of You",
+        artist: "music-17",
+        img: "Music-17",
+        src: "Music-17"
+    }
+    ,
+    {
+        name: "Stay",
+        artist: "music-18",
+        img: "Music-18",
+        src: "Music-18"
+    }
+    ,
+    {
+        name: "Blank Space",
+        artist: "music-19",
+        img: "Music-19",
+        src: "Music-19"
+    }
+    ,
+    {
+        name: "Bad Blood",
+        artist: "music-20",
+        img: "Music-20",
+        src: "Music-20"
+    },
+    {
+        name: "Backboner",
+        artist: "music-21",
+        img: "Music-21",
+        src: "Music-21"
+    },
+    {
+        name: "Bijlee-Bijlee",
+        artist: "music-22",
+        img: "Music-22",
+        src: "Music-22"
+    },
+    {
+        name: "ButterFly",
+        artist: "music-23",
+        img: "Music-23",
+        src: "Music-23"
+    },
+    {
+        name: "High Rated Gabru",
+        artist: "music-24",
+        img: "Music-24",
+        src: "Music-24"
+    },
+    {
+        name: "Laung lachi",
+        artist: "music-25",
+        img: "Music-25",
+        src: "Music-25"
+    },
+    {
+        name: "Patola",
+        artist: "music-26",
+        img: "Music-26",
+        src: "Music-26"
+    }
+    ,
+    {
+        name: "Sakhiyan",
+        artist: "music-27",
+        img: "Music-27",
+        src: "Music-27"
+    }
+    ,
+    {
+        name: "Same Beef",
+        artist: "music-28",
+        img: "Music-28",
+        src: "Music-28"
+    }
+    ,
+    {
+        name: "Suit Punjabi",
+        artist: "music-29",
+        img: "Music-29",
+        src: "Music-29"
+    }
+    ,
+    {
+        name: "Titliaan",
+        artist: "music-30",
+        img: "Music-30",
+        src: "Music-30"
+    }
+];
